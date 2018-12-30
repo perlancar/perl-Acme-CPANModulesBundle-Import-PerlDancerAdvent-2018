b81f968c4eaf490f59f2ddc0beb448832be9ff5c
perlancar (on netbook-zenbook-ux305) <perlancar@gmail.com>
2018-12-30 07:56:04 +0700

    Release v0.001

--- lib/Acme/CPANModulesBundle/Import/PerlDancerAdvent/2016.pm
+++
@@ -1,9 +0,0 @@
-package Acme::CPANModulesBundle::Import::PerlDancerAdvent::2016;
-
-# DATE
-# VERSION
-
-1;
-# ABSTRACT: Modules mentioned in PerlDancer Advent Calendar 2016
-
-=head1 DESCRIPTION
---
+++ lib/Acme/CPANModulesBundle/Import/PerlDancerAdvent/2018.pm
@@ -0,0 +1,9 @@
+package Acme::CPANModulesBundle::Import::PerlDancerAdvent::2018;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: Modules mentioned in PerlDancer Advent Calendar 2018
+
+=head1 DESCRIPTION
